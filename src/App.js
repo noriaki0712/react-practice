a17c269218fc49343ecb4c58520421f843bb3346
noriaki_kamitake <noriaki_kamitake@vitalize.co.jp>
2021-05-08 16:47:43 +0900

    jsx

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={()=> {console.log("I am clicked!")}}/>
+      </React.Fragment>
+    )
+  }
+}
+
+export default App;
+
