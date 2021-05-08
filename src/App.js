8cd504d7a50f171f6c2337b58cfa934f7a0a11e6
noriaki_kamitake <noriaki_kamitake@vitalize.co.jp>
2021-05-08 17:24:36 +0900

    props

--- src/App.js
+++ src/App.js
@@ -13,15 +13,26 @@ import './App.css';
 // }
 
 const App = () => {
-  return <div>
-    <Cat />
-    <Cat />
-    <Cat />
-    <Cat />
+  const profiles = [
+    { name: "Taro", age: 10},
+    { name: "Hanako", age: 5},
+    { name: "Kyoko"}
+  ]
+  return(
+  <div>
+    {
+      profiles.map((profile, index) => {
+        return  <User name={profile.name} age={profile.age} key={index}/>
+      })
+    }
   </div>
+  )
 }
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi, i am {props.name} and {props.age} years old</div>
+}
+User.defaultProps = {
+  age: 0
 }
 export default App;
 
