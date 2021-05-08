cb116fdc60182eb5b4184a5accd8b37e9e4b7b04
noriaki_kamitake <noriaki_kamitake@vitalize.co.jp>
2021-05-08 17:05:16 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
-import React, { Component } from 'react'
+// import React, * as react from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={()=> {console.log("I am clicked!")}}/>
-      </React.Fragment>
-    )
-  }
+// class app extends component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={()=> {console.log("I am clicked!")}}/>
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+const App = () => {
+  return <div>
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+  </div>
 }
-
+const Cat = () => {
+  return <div>Meow!</div>
+}
 export default App;
 
